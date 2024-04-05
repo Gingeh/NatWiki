e732ef263bde9d9884cd86ce3902e103a33acd07
Gingeh <39150378+Gingeh@users.noreply.github.com>
2024-04-05 17:39:14 +1100

    use rug instead of num-bigint

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use std::sync::Arc;
 
 use askama::Template;
 use axum::{extract::Path, http::StatusCode, routing::get, Router};
-use num::{BigUint, Num};
+use rug::Integer;
 use tokio::task::JoinSet;
 
 mod nerds;
@@ -18,12 +18,12 @@ async fn main() {
 #[derive(Template)]
 #[template(path = "int.html")]
 struct IntTemplate {
-    n: Arc<BigUint>,
+    n: Arc<Integer>,
     facts: Vec<String>,
 }
 
 async fn handle_int(Path(param): Path<String>) -> Result<IntTemplate, (StatusCode, String)> {
-    let Ok(n) = BigUint::from_str_radix(&param, 10) else {
+    let Ok(n) = Integer::parse(&param).map(rug::Complete::complete) else {
         return Err((
             StatusCode::BAD_REQUEST,
             format!("Error: {param:?} could not be parsed as an unsigned integer."),
@@ -44,6 +44,6 @@ async fn handle_int(Path(param): Path<String>) -> Result<IntTemplate, (StatusCod
             facts.push(fact);
         }
     }
-    
+
     Ok(IntTemplate { n, facts })
 }
--- src/nerds.rs
+++ src/nerds.rs
@@ -1,30 +1,22 @@
-use num::BigUint;
 use std::sync::Arc;
 
-pub const NERDS: &[fn(Arc<BigUint>) -> Option<String>] = &[even_or_odd, triangular_number];
+use rug::Integer;
 
-pub fn even_or_odd(n: Arc<BigUint>) -> Option<String> {
-    Some(if n.bit(0) {
-        "Is an odd number.".to_string()
-    } else {
+pub const NERDS: &[fn(Arc<Integer>) -> Option<String>] = &[even_or_odd, triangular_number];
+
+fn even_or_odd(n: Arc<Integer>) -> Option<String> {
+    Some(if n.is_even() {
         "Is an even number.".to_string()
+    } else {
+        "Is an odd number.".to_string()
     })
 }
 
-pub fn triangular_number(n: Arc<BigUint>) -> Option<String> {
-    let disc_sq = Arc::unwrap_or_clone(n) * 8_u8 + 1_u8;
-    let disc = disc_sq.sqrt();
-
-    // 8x+1 must be a square
-    if disc.pow(2) != disc_sq {
+fn triangular_number(n: Arc<Integer>) -> Option<String> {
+    let (disc, rem) = (Arc::unwrap_or_clone(n) * 8_u8 + 1_u8).sqrt_rem(Integer::new());
+    if !rem.is_zero() || disc.is_even() {
         return None;
     }
-
-    // sqrt(8x+1)-1 must be even
-    if !disc.bit(0) {
-        return None;
-    }
-
-    let root = (disc - 1_u8) / 2_u8;
+    let root = (disc - 1) / 2;
     Some(format!("Is the {root}th triangular number."))
 }
